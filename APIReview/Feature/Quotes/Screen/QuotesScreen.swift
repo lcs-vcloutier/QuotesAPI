4d6f928348f614a0ad1301de1bb97e958c5eca90
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-06 09:56:37 -0400

    connected view model to quotes screen - and added progress view while we wait

--- APIReview/Feature/Quotes/Screen/QuotesScreen.swift
+++ APIReview/Feature/Quotes/Screen/QuotesScreen.swift
@@ -9,15 +9,28 @@ import SwiftUI
 
 @available(iOS 15, *)
 struct QuotesScreen: View {
+    
+    @StateObject private var vm = QuotesViewModelImpl(service: QuotesServiceImpl())
+    
     var body: some View {
-        List {
-            // Loop through dummy data
-            ForEach(Quote.dummyData, id: \.anime) { item in
-                QuoteView(item: item)
-                .padding()
-                .foregroundColor(.black)
+        
+        Group {
+            if vm.quotes.isEmpty {
+                VStack(spacing: 8) {
+                    ProgressView()
+                    Text("Fetching Quotes")
+                }
+            } else {
+                List {
+                    // Loop through dummy data
+                    ForEach(vm.quotes, id: \.anime) { item in
+                        QuoteView(item: item)
+                    }
+                }
             }
-            
+        }
+        .task {
+            await vm.getRandomQuotes()
         }
     }
 }
