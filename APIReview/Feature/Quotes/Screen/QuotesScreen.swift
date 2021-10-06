7bed9e538e7951d8a2c9848519d61d358897caec
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-06 09:26:07 -0400

    extracted quote view from screen

--- APIReview/Feature/Quotes/Screen/QuotesScreen.swift
+++ APIReview/Feature/Quotes/Screen/QuotesScreen.swift
@@ -13,34 +13,12 @@ struct QuotesScreen: View {
         List {
             // Loop through dummy data
             ForEach(Quote.dummyData, id: \.anime) { item in
-                VStack(alignment: .leading, spacing: 8) {
-                    HStack {
-                        Image(systemName: "tv")
-                            .font(.system(size: 12, weight: .black))
-                        Text(item.anime)
-                    }
-                    Text(makeAttributedString(title: "Character", label: item.character))
-                    Text(makeAttributedString(title: "Quotes", label: item.quote))
-                        .lineLimit(2)
-                }
+                QuoteView(item: item)
                 .padding()
                 .foregroundColor(.black)
             }
             
         }
-    }
-    // Make a nice string
-    private func makeAttributedString(title: String, label: String) -> AttributedString {
-        
-        var string = AttributedString("\(title): \(label)")
-        string.foregroundColor = .black
-        string.font = .system(size: 16, weight: .bold)
-        
-        if let range = string.range(of: label) {
-            string[range].foregroundColor = .black.opacity(0.8)
-            string[range].font = .system(size: 16, weight: .bold)
-        }
-        return string
     }
 }
 
