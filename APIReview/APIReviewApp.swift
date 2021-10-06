7abdc9839c693ef010062ac051baab1e5bd06f61
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-06 08:52:17 -0400

    Initial Commit

---
+++ APIReview/APIReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIReviewApp.swift
+//  APIReview
+//
+//  Created by Vincent Cloutier on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct APIReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
