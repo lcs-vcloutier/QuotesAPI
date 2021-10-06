a6d879fdaf186e02645b3423e35b9e6f4759d091
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-06 09:04:56 -0400

    added dummy data

--- APIReview/Feature/Quotes/Model/Quote.swift
+++ APIReview/Feature/Quotes/Model/Quote.swift
@@ -13,3 +13,14 @@ struct Quote: Decodable {
     let character: String
     let quote: String
 }
+
+extension Quote {
+    
+    static let dummyData: [Quote] = [
+        Quote(anime: "Anime 1", character: "Character 1", quote: "Quote 1"),
+        Quote(anime: "Anime 2", character: "Character 2", quote: "Quote 2"),
+        Quote(anime: "Anime 3", character: "Character 3", quote: "Quote 3"),
+        Quote(anime: "Anime 4", character: "Character 4", quote: "Quote 4"),
+        Quote(anime: "Anime 5", character: "Character 5", quote: "Quote 5S")
+    ]
+}
